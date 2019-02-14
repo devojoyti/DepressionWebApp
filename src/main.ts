67f5499cf228922a258aa8742cc2c0f3fa58eb21
Dev Halder <dehalder@microsoft.com>
2019-02-14 12:49:33 +0530

    removed jquery

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
-import Vue from 'vue';
-import App from './App.vue';
-import store from './store';
-import './../node_modules/bulma/css/bulma.css';
-import $ from "jquery"; 
+import Vue from "vue";
+import App from "./App.vue";
+import store from "./store";
+import "./../node_modules/bulma/css/bulma.css";
 
 Vue.config.productionTip = false;
 
 new Vue({
     store,
-    render: (h) => h(App),
-}).$mount('#app');
+    render: h => h(App),
+}).$mount("#app");
